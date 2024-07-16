6521b9948ef37474ff35849236173358b051e985
Tran Anh Tuan <tuantahe179007@fpt.edu.vn>
2024-07-16 22:21:03 +0700

    Update MainContent.js

--- project_fer202/src/components/MainContent.js
+++ project_fer202/src/components/MainContent.js
@@ -1,15 +1,30 @@
 import { Row, Col, Card, Button, Tab, Tabs, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import "../style/MainContent.css";
 
-export default function MainContent({ showingmovies = [], comingmovies = [] }) {
+export default function MainContent() {
+    const [showingmovies, setShowingmovies] = useState([]);
+    const [comingmovies, setComingmovies] = useState([]);
+
+    useEffect(() => {
+        //GER - URI: http://localhost:9999/movies
+        fetch("http://localhost:9999/movies")
+            .then(res => res.json())
+            .then(result => setShowingmovies(result))
+            .catch(error => console.log(error));
+        fetch("http://localhost:9999/comingmovies")
+            .then(res => res.json())
+            .then(result => setComingmovies(result))
+            .catch(error => console.log(error));
+
+    }, []);
+
     return (
         <Col>
             <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
                 <Tab eventKey="home" title="Phim Sắp Chiếu" className="tab-movie">
                     <Container>
                         <Row sm={2} xs={1} md={4} className="g-4">
-<<<<<<< Updated upstream
-=======
                             {comingmovies.map((comingmovie) => (
                                 <Col style={{ textAlign: "center" }}>
                                     <Card>
@@ -17,9 +32,9 @@ export default function MainContent({ showingmovies = [], comingmovies = [] }) {
                                             <Card.Img variant="top" src={comingmovie.imageSrc} className="card-img" />
                                         </div>
                                         <Card.Body>
-                                            <Card.Text><h3>{comingmovie.title}</h3></Card.Text>
+                                            <Card.Text>{comingmovie.title}</Card.Text>
                                             <Card.Text className="d-flex justify-content-between">
-                                                <span>{comingmovie.description}</span>
+                                                <span>{comingmovie.genres}</span>
                                             </Card.Text>
                                             <Button variant="primary" type="submit" style={{ backgroundColor: "#333333", maxWidth: "100%" }}>
                                                 Đặt suất chiếu sớm
@@ -34,7 +49,6 @@ export default function MainContent({ showingmovies = [], comingmovies = [] }) {
                 <Tab eventKey="profile" title="Phim Đang Chiếu" className="tab-movie">
                     <Container>
                         <Row sm={2} xs={1} md={4} className="g-4">
->>>>>>> Stashed changes
                             {showingmovies.map((showingmovie) => (
                                 <Col style={{ textAlign: "center" }}>
                                     <Card>
@@ -42,17 +56,12 @@ export default function MainContent({ showingmovies = [], comingmovies = [] }) {
                                             <Card.Img variant="top" src={showingmovie.imageSrc} className="card-img" />
                                         </div>
                                         <Card.Body>
-                                            <Card.Text><h3>{comingmovie.title}</h3></Card.Text>
+                                            <Card.Text>{showingmovie.title}</Card.Text>
                                             <Card.Text className="d-flex justify-content-between">
-<<<<<<< Updated upstream
-                                                <span>{showingmovie.description}</span>
-=======
-                                                Thể Loại
                                                 <span>{showingmovie.genres}</span>
->>>>>>> Stashed changes
                                             </Card.Text>
                                             <Button variant="primary" type="submit" style={{ backgroundColor: "#333333", maxWidth: "100%" }}>
-                                                Đặt Suất Chiếu Sớm
+                                                Đặt Vé
                                             </Button>
                                         </Card.Body>
                                     </Card>
@@ -60,30 +69,7 @@ export default function MainContent({ showingmovies = [], comingmovies = [] }) {
                             ))}
                         </Row>
                     </Container>
-                </Tab>
-                <Tab eventKey="profile" title="Phim Đang Chiếu" className="tab-movie">
-                    <Container>
-                        <Row sm={2} xs={1} md={4} className="g-4">
-                            {comingmovies.map((comingmovie) => (
-                                <Col style={{ textAlign: "center" }}>
-                                    <Card>
-                                        <div className="card-img-container">
-                                            <Card.Img variant="top" src={comingmovie.imageSrc} className="card-img" />
-                                        </div>
-                                        <Card.Body>
-                                            <Card.Text>{comingmovie.title}</Card.Text>
-                                            <Card.Text className="d-flex justify-content-between">
-                                                <span>{comingmovie.description}</span>
-                                            </Card.Text>
-                                            <Button variant="primary" type="submit" style={{ backgroundColor: "#333333", maxWidth: "100%" }}>
-                                                Đặt vé
-                                            </Button>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            ))}
-                        </Row>
-                    </Container>
+
                 </Tab>
             </Tabs>
         </Col >
