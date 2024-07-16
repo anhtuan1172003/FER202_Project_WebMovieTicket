1f608dc6855926ddf00d9928c13d8f85c8d4fac6
Tran Anh Tuan <tuantahe179007@fpt.edu.vn>
2024-07-16 22:20:01 +0700

    Update MainContent.js

--- project_fer202/src/components/MainContent.js
+++ project_fer202/src/components/MainContent.js
@@ -8,6 +8,33 @@ export default function MainContent({ showingmovies = [], comingmovies = [] }) {
                 <Tab eventKey="home" title="Phim Sắp Chiếu" className="tab-movie">
                     <Container>
                         <Row sm={2} xs={1} md={4} className="g-4">
+<<<<<<< Updated upstream
+=======
+                            {comingmovies.map((comingmovie) => (
+                                <Col style={{ textAlign: "center" }}>
+                                    <Card>
+                                        <div className="card-img-container">
+                                            <Card.Img variant="top" src={comingmovie.imageSrc} className="card-img" />
+                                        </div>
+                                        <Card.Body>
+                                            <Card.Text><h3>{comingmovie.title}</h3></Card.Text>
+                                            <Card.Text className="d-flex justify-content-between">
+                                                <span>{comingmovie.description}</span>
+                                            </Card.Text>
+                                            <Button variant="primary" type="submit" style={{ backgroundColor: "#333333", maxWidth: "100%" }}>
+                                                Đặt suất chiếu sớm
+                                            </Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Container>
+                </Tab>
+                <Tab eventKey="profile" title="Phim Đang Chiếu" className="tab-movie">
+                    <Container>
+                        <Row sm={2} xs={1} md={4} className="g-4">
+>>>>>>> Stashed changes
                             {showingmovies.map((showingmovie) => (
                                 <Col style={{ textAlign: "center" }}>
                                     <Card>
@@ -15,9 +42,14 @@ export default function MainContent({ showingmovies = [], comingmovies = [] }) {
                                             <Card.Img variant="top" src={showingmovie.imageSrc} className="card-img" />
                                         </div>
                                         <Card.Body>
-                                            <Card.Text>{showingmovie.title}</Card.Text>
+                                            <Card.Text><h3>{comingmovie.title}</h3></Card.Text>
                                             <Card.Text className="d-flex justify-content-between">
+<<<<<<< Updated upstream
                                                 <span>{showingmovie.description}</span>
+=======
+                                                Thể Loại
+                                                <span>{showingmovie.genres}</span>
+>>>>>>> Stashed changes
                                             </Card.Text>
                                             <Button variant="primary" type="submit" style={{ backgroundColor: "#333333", maxWidth: "100%" }}>
                                                 Đặt Suất Chiếu Sớm
